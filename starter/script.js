5e5299a0b6a38363530ac2c03225fa472c12f37f
aishwarya858 <aishwarya.cs25@sahyadri.edu.in>
2025-12-07 15:54:43 +0530

    Added animation and fixed the hidden bug successfully

--- starter/script.js
+++ starter/script.js
@@ -1,3 +1,19 @@
-document.getElementById("info-bnt").addEventListener("click", () => {
+document.getElementById("info-btn").addEventListener("click", () => {
     alert("JS effect working!");
 });
+const nameHeader = document.getElementById('name');
+const actionButton = document.getElementById('info-btn');
+if (actionButton) {
+    actionButton.addEventListener('click', function() {
+        nameHeader.classList.toggle('rainbow-mode');
+        
+        console.log("Rainbow mode toggled!");
+   });
+}
+
+
+    
+    
+    
+
+
